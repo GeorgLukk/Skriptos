3e5a2cfb2d71f119db0c73227776b8c89657e303
georg <georg.lukk@vikk.ee>
2024-01-09 13:42:38 +0200

    gg

---
+++ testipoiss.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Essa skriptipoiss
+#Autor: Georg
+#Muudetud 09.01.24
+
+
+kp=`date +%D.%T`
+echo "Olen sisse logitud $USER kasutajaga"
+echo "Mina olen Georg"
+echo "See on minu esimene skriptipoiss"
+echo
+echo "Täna on 09.01.24"
+echo "Täna on $kp" 
+date +%D_%T
